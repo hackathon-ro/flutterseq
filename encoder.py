6d8183b4c9e61e11323b354888639842d81a8ea1
root <root@raspberrypi.(none)>
2013-04-20 23:18:10 +0300

    encoder.py - simple quadrature encoder indexer class. pass the two values, get the update. still with debug messages hardware.py - higher level encoder interface class, reads a 1x16 PISO register bus, parses data to encoder. currently done for only a single encoder on bits 0-1

---
+++ encoder.py
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+class Encoder:
+  def __init__(self, reverse = False):
+    self.a = bool(0)
+    self.b = bool(0)
+    self.oa = bool(0)
+    self.ob = bool(0)
+    self.position = 0
+    self.reverse = reverse
+
+  def update(self, a, b):
+    if (self.a != self.oa or self.b != self.ob):
+      #print ("Val %1d-%1d") % (a,b)
+      if (self.reverse == True):
+        direction = self.a ^ self.ob
+      else:
+        direction = self.b ^ self.oa
+
+      if (direction == 1):
+        self.position += 1
+      elif (direction == 0):
+        self.position -= 1
+      #print "Dir", direction
+      print "Pos", self.position
+      #print 
+
+    self.oa = self.a
+    self.ob = self.b
+    self.a = a
+    self.b = b
+
+  def reset(self):
+    self.position = 0
